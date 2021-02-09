fc051861b77b19b1fed1c31dcb72d1cc57c9841d
Noah Syrkis <noah@syrkis.com>
2021-02-09 14:36:31 +0100

    add 2015 12 b py

--- 2015/12.sh
+++ 2015/12.sh
@@ -1,7 +1,15 @@
 cat 12.txt | tr '{}' '\n' | grep -v red | tr -Cs '0-9-' '+' | sed 's/+$//' | sed 's/^+//' > tmp.txt
 echo 'with open("tmp.txt", "r") as f:
     line = f.read() 
-    print(eval(line))' > 12.py
-python 12.py
-rm 12.py
+    print(eval(line))' > tmp.py
+python tmp.py
+rm tmp.py
 rm tmp.txt
+
+echo 'import json
+with open("12.txt", "r") as f:
+    data = json.load(f)
+    for d in data.items():
+        print(d)' > tmp.py
+python tmp.py
+rm tmp.py
